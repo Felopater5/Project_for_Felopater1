1262b5418f24e62ffec3465ee22760f85b0a9682
Felopater Nader <felo.nader5@gmail.com>
2023-01-26 21:27:19 +0200

    first

---
+++ Project_for_Felopater1/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_for_Felopater1
+{
+    public class Functions
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string Constr;
+
+    }
+}
--- Project_for_Felopater1/Program.cs
+++ Project_for_Felopater1/Program.cs
@@ -16,7 +16,7 @@ namespace Project_for_Felopater1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form());
         }
     }
 }
