9e487e44958b3ed8cc63ee74f8076218d1981159
Felopater Nader <felo.nader5@gmail.com>
2023-01-26 22:33:46 +0200

    create Button DeleteBtn

--- Project_for_Felopater1/Deparment.Designer.cs
+++ Project_for_Felopater1/Deparment.Designer.cs
@@ -54,5 +54,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button DeleteBtn;
     }
 }
