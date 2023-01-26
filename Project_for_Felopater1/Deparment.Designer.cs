bc97c3011621a51d497addfcd1745ec4e0046a3e
Felopater Nader <felo.nader5@gmail.com>
2023-01-26 22:33:01 +0200

    create label

--- Project_for_Felopater1/Deparment.Designer.cs
+++ Project_for_Felopater1/Deparment.Designer.cs
@@ -50,5 +50,9 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label EmpPage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
